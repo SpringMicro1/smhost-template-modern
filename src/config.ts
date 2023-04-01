939124619b677f16b0d88858c307cf27385c936d
Richard Henage <rthenage@gmail.com>
2023-04-01 14:33:59 -0600

    Copy from demo folder, IMAGES, LINKS, PARAGRAPHS

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+const pagesString = 
+// BEGIN JSON
+`{"BASE":{"SITE_TITLE":"Astro Modern Personal Website Template","SITE_DESCRIPTION":"Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Design."},"SIDEBAR":{"TEXT":{},"LINKS":{},"IMAGES":{"PROFILE":{"src":"https://avatars.githubusercontent.com/u/30419176?v=4","alt":"profile picture"}}},"HOME":{"TEXT":{"SALUTATION":"Continuity test. Hello there 👋","H1":"I'm David Buckley","SUBTITLE":"CS Teacher and Software Engineer","INTRODUCTION":"I made this template as a demonstration of loading content from a config file!", "PARAGRAPHS": {"Paragraph 1": "This is a paragraph"}},"LINKS":{"Link 1":{"href":"https://www.linkedin.com/in/buckldav/","content":"Let's connect!"}},"IMAGES":{}}}`;
+// END JSON
+const pagesObject = JSON.parse(pagesString);
+export const BASE = pagesObject.BASE
+export const SIDEBAR = pagesObject.SIDEBAR
+export const HOME = pagesObject.HOME
